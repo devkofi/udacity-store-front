a3e50305bac05d78473282d2990fecee8eb43437
Kofi Nyarko Kumi <kofinyarkokumi@gmail.com>
2023-01-28 08:13:45 +0000

    modified server

--- src/server.ts
+++ src/server.ts
@@ -7,14 +7,13 @@ const port = 3000;
 // const widths: number[] = [];
 // const heights: number[] = [];
 const rootFolder: string = path.resolve(__dirname);
-const optimizedImgPath = rootFolder + path.normalize("/public/img/optimized/");
-const originalImgPath = rootFolder + path.normalize("/public/img/original/");
-const file = fs.readdirSync(originalImgPath)[0];
-let accessibleFile = "";
+const views = rootFolder + path.normalize("/views/");
+//const originalImgPath = rootFolder + path.normalize("/public/img/original/");
+//const file = fs.readdirSync(originalImgPath)[0];
 
 app.use(express.static(rootFolder));
 
-const processImage = function (
+const middleware = function (
   req: express.Request,
   res: express.Response,
   next: express.NextFunction
@@ -27,28 +26,6 @@ app.get("/", (req: express.Request, res: express.Response) => {
   res.sendFile(rootFolder + path.normalize("/public/displayImage.html"));
 });
 
-app.use(processImage);
-
-app.get("/api/images", (req: express.Request, res: express.Response) => {
-  try {
-    console.log("AccessibleFile: " + accessibleFile);
-
-    const display = async (): Promise<void> => {
-      const myPromise = new Promise((resolve) => {
-        resolve(
-          setTimeout(() => {
-            res.sendFile(accessibleFile);
-          }, 100)
-        );
-      });
-      await myPromise;
-    };
-
-    display();
-  } catch (err) {
-    console.error(err);
-  }
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
