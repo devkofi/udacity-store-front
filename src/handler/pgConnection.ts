16c252bf8feff9e97a250d5e3e2dc0aabf92a1e5
Kofi Nyarko Kumi <kofinyarkokumi@gmail.com>
2023-02-12 07:09:54 +0000

    rename connection

---
+++ src/handler/pgConnection.ts
@@ -0,0 +1,58 @@
+import {Pool} from 'pg';
+import dotenv from 'dotenv'
+
+dotenv.config()
+const {
+    POSTGRES_HOST,
+    POSTGRES_PORT,
+    POSTGRES_DB,
+    POSTGRES_TEST_DB,
+    POSTGRES_USER,
+    POSTGRES_PASSWORD,
+    ENV
+} = process.env 
+
+
+export const connection = (): Pool =>{
+    const conn = ENV === "dev" ? new Pool({
+        host: POSTGRES_HOST,
+        port: Number(POSTGRES_PORT),
+        database: POSTGRES_DB,
+        user: POSTGRES_USER,
+        password: POSTGRES_PASSWORD,
+    }) : new Pool({
+        host: POSTGRES_HOST,
+        port: Number(POSTGRES_PORT),
+        database: POSTGRES_TEST_DB,
+        user: POSTGRES_USER,
+        password: POSTGRES_PASSWORD,
+    });
+
+    return conn;
+}
+
+
+// let connection;
+
+// if (ENV === "dev") {
+//     connection = new Pool({
+//         host: POSTGRES_HOST,
+//         port: Number(POSTGRES_PORT),
+//         database: POSTGRES_DB,
+//         user: POSTGRES_USER,
+//         password: POSTGRES_PASSWORD,
+//     });
+//   }
+  
+//   if (ENV === "test") {
+//     connection = new Pool({
+//         host: POSTGRES_HOST,
+//         port: Number(POSTGRES_PORT),
+//         database: POSTGRES_TEST_DB,
+//         user: POSTGRES_USER,
+//         password: POSTGRES_PASSWORD,
+//     });
+//   }
+
+//   export default connection;
+
