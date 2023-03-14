d0dfe470eeef794a55ec842bf6306fb2bde56900
Kofi Nyarko Kumi <kofinyarkokumi@gmail.com>
2023-03-14 21:30:09 +0000

    change quantity to integer

--- migrations/sqls/20230314205139-order-products-migration-up.sql
+++ migrations/sqls/20230314205139-order-products-migration-up.sql
@@ -3,5 +3,5 @@ CREATE TABLE orders_products(
     id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES users(id) NOT NULL,
     product_id INTEGER REFERENCES products(id) NOT NULL,
-    quantity INTEGER REFERENCES orders(product_quantity) NOT NULL
+    quantity INTEGER NOT NULL
 );
